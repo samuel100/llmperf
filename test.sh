beaf66cfeee0427dfa97776668ce927b3fd3f40e
samuel100 <samkemp@microsoft.com>
2024-02-26 22:29:31 +0000

    init

---
+++ test.sh
@@ -0,0 +1,12 @@
+python token_benchmark_ray.py \
+--model "llama-2-70b-chat" \
+--mean-input-tokens 550 \
+--stddev-input-tokens 0 \
+--mean-output-tokens 150 \
+--stddev-output-tokens 0 \
+--max-num-completed-requests 150 \
+--timeout 600 \
+--num-concurrent-requests 5 \
+--results-dir "result_outputs" \
+--llm-api "azureaistudio" \
+--additional-sampling-params '{}'
